9cca3e385c6db2c8acd0e6812b5f570e5596acf9
Vedat ERMİŞ <vedatermis@hotmail.com>
2022-07-11 21:01:06 +0300

    Initial Commit

---
+++ API/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnectionString"));
+});
+
+var app = builder.Build();
+
+await using var scope = app.Services.CreateAsyncScope();
+
+var dataContext = scope.ServiceProvider.GetRequiredService<DataContext>();
+await dataContext.Database.MigrateAsync();
+await Seed.SeedData(dataContext);
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+await app.RunAsync();
