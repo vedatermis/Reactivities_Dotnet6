babc9db4c3e0a4d32a5c9fdefd7051da691722a5
Vedat ERMİŞ <vedatermis@hotmail.com>
2022-07-19 19:50:11 +0300

    commit

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,26 +1,33 @@
 import React, { Fragment, useEffect, useState } from "react";
 import axios from "axios";
-import { Container, Header, List } from "semantic-ui-react";
+import { Container } from "semantic-ui-react";
 import Activity from "../models/activity";
 import NavBar from "./NarBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
 
   useEffect(() => {
     axios.get("http://localhost:5000/api/Activities").then((response) => {
-      console.log(response);
-      console.log(response.data);
       setActivities(response.data);
     });
   }, []);
+
+  function handleSelectedActivity(id: string) {
+      setSelectedActivity(activities.find(x => x.id === id));
+  }
+
+  function handleCancelSelectedActivity() {
+      setSelectedActivity(undefined);
+  }
 
   return (
     <Fragment>
       <NavBar />
       <Container style={{ marginTop: "7em" }}>
-        <ActivityDashboard activities={activities} />
+        <ActivityDashboard activities={activities} selectedActivity={selectedActivity} selectActivity={handleSelectedActivity} cancelSelectActivity={handleCancelSelectedActivity} />
       </Container>
     </Fragment>
   );
